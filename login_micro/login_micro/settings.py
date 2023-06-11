e29fbc1d5e7550e9f78c233b0911999c819b52d9
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-06-11 14:58:22 -0500

    Created superuser for Django /admin panel
    All seems to be working as expected.

--- login_micro/login_micro/settings.py
+++ login_micro/login_micro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'login.User'
