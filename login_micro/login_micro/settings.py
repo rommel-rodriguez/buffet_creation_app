0db087e2dfd36eda13643a790cfa47b43ca7c5fd
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-06-13 10:45:00 -0500

    All tests working
    Up to me/ endpoint, everything seems to be workinng fine.

--- login_micro/login_micro/settings.py
+++ login_micro/login_micro/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
